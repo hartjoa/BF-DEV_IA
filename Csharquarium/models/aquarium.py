a220d5631920a93f72b5ec089d712b857107996d
hartjoa <hartjoa@gmail.com>
2025-07-18 13:21:10 +0200

    added fishe's feeding and death

--- Csharquarium/models/aquarium.py
+++ Csharquarium/models/aquarium.py
@@ -31,27 +31,46 @@ class Aquarium:
         for alga in self.algae:
             alga.pv += 1
         
-        # all the fishes eat
+        # hunger fishes eat
         for fish in self.fishes:
-            food = None
-            if isinstance(fish, HerbivorousFish):
-                if len(self.algae) < 1:
-                    print(f"There's no more alga left to feed you, poor {fish.name}")
-                    continue
-                food = random.choice(self.algae)
-                self.alga_eaten(food)
-            elif isinstance(fish, CarnivorousFish):
-                if len(self.fishes) < 2:
-                    print(f"There's no more fish left to feed you, poor {fish.name}")
-                    break
-                while not food or food == fish: # cannot eat yourself
-                    food = random.choice(self.fishes)
-                    if food != fish:
-                        self.fishes.remove(food)
-            else:
-                raise TypeError("Any fish should be either carnivorous or herbivorous")
+            fish.pv -= 1
+            if fish.pv <= 5:
+                # fish is hungry
+                if isinstance(fish, HerbivorousFish):
+                    # herbivorous
+                    print(f"{fish.name} is looking for an alga to eat...")
+                    if len(self.algae) == 0:
+                        # no more alga in the aquarium
+                        print(f"There's no more alga left to feed you, poor {fish.name}")
+                        continue
+                    # eat an alga
+                    alga = random.choice(self.algae)
+                    # alga is harmed
+                    alga.pv -= 2
+                    # fish is fed
+                    fish.pv += 3
+                elif isinstance(fish, CarnivorousFish):
+                    if len(self.fishes) < 2:
+                        # only fish in the aquarium
+                        print(f"There's no more fish left to feed you, poor {fish.name}")
+                        continue
+                    # eat a fish
+                    while not prey or prey == fish: # cannot eat yourself
+                        prey = random.choice(self.fishes)
+                        if prey != fish:
+                            # prey is harmed
+                            prey.pv -= 4
+                    # fish is fed
+                    fish.pv += 5
+                else:
+                    raise TypeError("Any fish should be either carnivorous or herbivorous")
         
         # all the dead algae dissapear
         dead_algae = [alga for alga in self.algae if not alga.is_alive]
         for dead_alga in dead_algae:
             self.algae.remove(dead_alga)
+        
+        # all the dead fishes dissapear
+        dead_fishes = [fish for fish in self.fishes if not fish.is_alive]
+        for dead_fish in dead_fishes:
+            self.fiches.remove(dead_fish)
